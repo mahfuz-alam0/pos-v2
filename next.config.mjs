a1d2cba5fa8215716af16c26f64758113c465a31
Mahfuz Alam <hasibul112002@gmail.com>
2026-07-22 01:57:34 +0600

    feat: update API client base URLs to use environment variables and remove outdated rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,6 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/pos/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-      },
-      {
-        source: "/api/ecom/:path*",
-        destination: `${process.env.NEXT_PUBLIC_ECCOMMERCE_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
